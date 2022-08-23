c3cd59e0e0879797529e152f17d3959826b36597
Dr Ross Campbell <36095902+drcampbell1@users.noreply.github.com>
2022-08-23 11:18:57 +0100

    Update Lab_5_Social_Capital.R

--- Lab Exercises/Lab_5_Social_Capital.R
+++ Lab Exercises/Lab_5_Social_Capital.R
@@ -1,5 +1,5 @@
-# In this lab, we're going to follow-up and examine some of the trends in social capital.
-#Of all the forms of participation, this is by far the hardest to measure. So we need
+# In this lab, we're going to follow-up and examine the trends and consequences of social capital.
+# Of all the forms of participation, this is by far the hardest to measure. So we need
 # remember that our findings are limited.
 
 # Let's start by looking at the data:
@@ -8,46 +8,63 @@ library(tidyverse)
 library(kableExtra)
 options(warn = -1)
 
-# Question 1: How social are we?
+# Question 1: How trusting are we?
 
-# We're going to start by looking at the variable "social". This is a question asking how
-# frequently people met with their friends.
+# We're going to start by looking at the variable measuring social trust. This is a question asking how
+# trusting we are of other people and is perhaps the most widely used indicator of social capital.
 
-ess %>% filter(!is.na(social)) %>% count(social) %>%
+ess %>% filter(!is.na(strust)) %>% count(strust) %>%
   mutate(percent = round(n/sum(n)*100, digits=1)) %>%
-  kbl("pandoc", caption = "Socialise with Friends in European Democracies",
-  col.names = c('Socialise', 'N', '%'), align="lccc")%>%
+  kbl(caption = "Socialise with Friends in European Democracies",
+  col.names = c('Social Trust', 'N', '%'), align="lccc")%>%
+  kable_classic_2(full_width=F, position= "left")%>%
+  footnote(general = "Source: ESS 2002-2018")
 
-# Fewer people socialise less than once a week, so let's find out how numerous the more socially active people
-# are in our countries. To do this I've created a new variable "socialise", which combines never/rare and 1-2 per month into one
-# category and 1 per week and more than once per week into one category.
+# The pattern varies, but it's possible that a lot of the variation comes from countries with particularly low levels of social trust.
+# We need to compare the different countries within our sample. Lets take a look:
 
-ESS %>% filter(!is.na(socialise)) %>% group_by(country) %>%
-              count(socialise) %>% mutate(percent = round(n/sum(n)*100, digits=1)) %>%
-              knitr::kable("pandoc", caption = "Socialise with Friends in European Democracies",
-              col.names = c('Country', 'Socialise', 'N', '%'), align="lcccc")
+ess %>% filter(!is.na(strust)) %>% group_by(country) %>%
+              count(strust) %>% mutate(percent = round(n/sum(n)*100, digits=1)) %>%
+              kbl(caption = "Socialise with Friends in European Democracies",
+              col.names = c('Country', 'Socialise', 'N', '%'), align="lcccc") %>%
+              kable_classic_2(full_width=F, position= "left")%>%
+              footnote(general = "Source: ESS 2002-2018")
 
-# We can graph this to make it clear
+# And we can graph this to make it clear
 
-ESS %>% group_by(cntry) %>% filter(!is.na(socialise)) %>%
-  count(socialise) %>% mutate(prop=prop.table(n*100)) %>%
-  filter(!socialise=="rare") %>%
-  ggplot(aes(x=reorder(cntry, -prop), y=prop, fill=cntry)) +
+ess %>% group_by(country) %>% filter(!is.na(strust)) %>%
+  count(strust) %>% mutate(prop=prop.table(n*100)) %>%
+  filter(!strust=="low" & !strust == "medium") %>%
+  ggplot(aes(x=reorder(cntry, -prop), y=prop, fill=country)) +
   geom_bar(stat="identity")+
   scale_fill_manual(values=c("#E69F00", "#009E73", "#0072B2", "#D55E00", "#56B4E9", "#CC79A7"))+
-  labs(x="", y="", title="Figure 1: Socialising by Country", caption="ESS 2016")+
+  labs(x="", y="", title="Figure 1: Social Trust by Country", caption="Source: ESS 2002-2018")+
   scale_y_continuous(labels=scales::percent)+
   theme_bw()+
   guides(fill=FALSE)
 
+# Has anything changed over time?
+
+ess %>% group_by(year, country) %>% filter(!is.na(strust)) %>%
+  count(strust) %>% mutate(prop=prop.table(n*100)) %>%
+  filter(!strust=="low" & !strust == "medium") %>%
+  ggplot(aes(year, prop))+
+  geom_line()+
+  facet_wrap(~country, nrow = 3)+
+  labs(x="", y="", title="Figure 2: Social Trust Over Time", caption="ESS 2002-2018")+
+  scale_y_continuous(labels=scales::percent)+
+  theme_bw()+
+  scale_x_continuous(breaks = c(2002, 2006, 2010, 2014, 2018))
+
+
 # Do the figures differ by age category?
 
-ESS %>% group_by(agecat) %>% filter(!is.na(socialise), !is.na(agecat)) %>%
-  count(socialise) %>% mutate(prop=prop.table(n*100)) %>%
-  filter(!socialise=="rare") %>%
+ess %>% group_by(agecat) %>% filter(!is.na(strust), !is.na(strust)) %>%
+  count(strust) %>% mutate(prop=prop.table(n*100)) %>%
+  filter(!strust=="low" & !strust == "medium") %>%
   ggplot(aes(x=agecat, y=prop)) +
   geom_bar(stat="identity")+
-  labs(x="", y="", title="Figure 2: Socialising by Age Category", caption="ESS 2016")+
+  labs(x="", y="", title="Figure 3: Social Trust by Age Category", caption="Source: ESS 2002-2018")+
   scale_y_continuous(labels=scales::percent)+
   theme_bw()+
   guides(fill=FALSE)
@@ -55,23 +72,23 @@ ESS %>% group_by(agecat) %>% filter(!is.na(socialise), !is.na(agecat)) %>%
 
 # Do the figures differ by Education
 
-ESS %>% group_by(educat) %>% filter(!is.na(socialise), !is.na(educat)) %>%
-  count(socialise) %>% mutate(prop=prop.table(n*100)) %>%
-  filter(!socialise=="rare") %>%
+ess %>% group_by(educat) %>% filter(!is.na(strust), !is.na(educat)) %>%
+  count(strust) %>% mutate(prop=prop.table(n*100)) %>%
+  filter(!strust=="low" & !strust == "medium") %>%
   ggplot(aes(x=educat, y=prop)) +
   geom_bar(stat="identity")+
-  labs(x="", y="", title="Figure 3: Socialising and Education", caption="ESS 2016")+
+  labs(x="", y="", title="Figure 4: Social Trust and Education", caption="ESS 2016")+
   scale_y_continuous(labels=scales::percent)+
   theme_bw()+
   guides(fill=FALSE)
 
 # And Gender
-ESS %>% group_by(gender) %>% filter(!is.na(socialise)) %>%
+ess %>% group_by(gender) %>% filter(!is.na(strust), !is.na(gender)) %>%
   count(socialise) %>% mutate(prop=prop.table(n*100)) %>%
-  filter(!socialise=="rare") %>%
+  filter(!strust=="low" & !strust == "medium") %>%
   ggplot(aes(x=gender, y=prop)) +
   geom_bar(stat="identity")+
-  labs(x="", y="", title="Figure 4: Socialising and Gender", caption="ESS 2016")+
+  labs(x="", y="", title="Figure 5: Social Trust and Gender", caption="Source: ESS 2002-2018")+
   scale_y_continuous(labels=scales::percent)+
   theme_bw()+
   guides(fill=FALSE)
@@ -84,94 +101,102 @@ ESS %>% group_by(gender) %>% filter(!is.na(socialise)) %>%
 
 # Voting
 
-ESS %>% filter(!is.na(socialise), !is.na(vote1)) %>%
-  group_by(socialise) %>% count(vote1) %>%
+ess %>% filter(!is.na(strust), !is.na(vote)) %>%
+  group_by(strust) %>% count(vote) %>%
   mutate(prop=n/sum(n)*100) %>%
-  filter(!vote1 == "did not vote") %>%
-  knitr::kable("pandoc", caption = "Socialising and Voting",
+  filter(!vote == "did not vote") %>%
+  kbl caption = "Social Trust and Electoral Turnout",
   col.names = c('Socialising', 'Voting', 'N', 'Percentage'),
-  digits=2, align="cccc") %>% print()
+  digits=2, align="cccc") %>%
+  kable_classic_2(full_width=F, position= "left")%>%
+  footnote(general = "Source: ESS 2002-2018")
 
-ESS %>% filter(!is.na(socialise), !is.na(vote1)) %>%
-  group_by(socialise, cntry) %>% count(vote1) %>%
+ess %>% filter(!is.na(strust), !is.na(vote)) %>%
+  group_by(strust, country) %>% count(vote) %>%
   mutate(prop=n/sum(n)*100) %>%
   filter(!vote1 == "did not vote") %>%
-  ggplot(aes(x=socialise, y=prop, fill=vote1)) +
+  ggplot(aes(x=strust, y=prop, fill=vote)) +
   geom_bar(stat="identity", position = "dodge")+
-  labs(x="", y="%", title="Figure 5: Voting by Socialising and Country", caption="ESS 2016")+
+  labs(x="", y="%", title="Figure 6: Voting by Socialising and Country", caption="ESS 2016")+
   theme_bw()+
-  facet_grid(~cntry)+
+  facet_grid(~country)+
   guides(fill=FALSE)
 
 # Contacting
 
-ESS %>% filter(!is.na(socialise), !is.na(contact)) %>%
-  group_by(socialise) %>% count(contact) %>%
+ess %>% filter(!is.na(strust), !is.na(contact)) %>%
+  group_by(strust) %>% count(contact) %>%
   mutate(prop=n/sum(n)*100) %>%
-  filter(!contact == "have not contacted") %>%
-  knitr::kable("pandoc", caption = "Socialising and Contacting Politicians",
+  filter(!contact == "not contacted") %>%
+  kbl(caption = "Socialising and Contacting Politicians",
   col.names = c('Socialising', 'Contacting', 'N', 'Percentage'),
-  digits=2, align="cccc") %>% print()
+  digits=2, align="cccc") %>%
+  kable_classic_2(full_width=F, position= "left")%>%
+  footnote(general = "Source: ESS 2002-2018")
 
 
 # Contacting across countries
 
-ESS %>% filter(!is.na(socialise), !is.na(contact)) %>%
-  group_by(socialise, cntry) %>% count(contact) %>%
+ess %>% filter(!is.na(strust), !is.na(contact)) %>%
+  group_by(strust, country) %>% count(contact) %>%
   mutate(prop=n/sum(n)*100) %>%
-  filter(!contact == "have not contacted") %>%
-  ggplot(aes(x=socialise, y=prop, fill=contact)) +
+  filter(!contact == "not contacted") %>%
+  ggplot(aes(x=strust, y=prop, fill=contact)) +
   geom_bar(stat="identity", position = "dodge")+
-  labs(x="", y="%", title="Figure 6: Contacting by Socialising and Country", caption="ESS 2016")+
+  labs(x="", y="%", title="Figure 7: Contacting by Socialising and Country", caption="Source: ESS 2002-2018")+
   theme_bw()+
-  facet_grid(~cntry)+
+  facet_grid(~country)+
   guides(fill=FALSE)
 
 # Petition
 
-ESS %>% filter(!is.na(socialise), !is.na(petition)) %>%
-  group_by(socialise) %>% count(petition) %>%
+ess %>% filter(!is.na(strust), !is.na(petition)) %>%
+  group_by(strust) %>% count(petition) %>%
   mutate(prop=n/sum(n)*100) %>%
   filter(!petition == "have not signed petition") %>%
-  knitr::kable("pandoc", caption = "Socialising and Petition",
+  kbl(caption = "Socialising and Petition",
   col.names = c('Socialising', 'Petition', 'N', 'Percentage'),
-  digits=2, align="cccc") %>% print()
+  digits=2, align="cccc")%>%
+  kable_classic_2(full_width=F, position= "left")%>%
+  footnote(general = "Source: ESS 2002-2018")
 
 # Petitioning across countries by socialising
 
-ESS %>% filter(!is.na(socialise), !is.na(petition)) %>%
-  group_by(socialise, cntry) %>% count(petition) %>%
+ess %>% filter(!is.na(strust), !is.na(petit)) %>%
+  group_by(strust, country) %>% count(petit) %>%
   mutate(prop=n/sum(n)*100) %>%
-  filter(!petition == "have not signed petition") %>%
-  ggplot(aes(x=socialise, y=prop, fill=petition)) +
+  filter(!petit == "not signed") %>%
+  ggplot(aes(x=strust, y=prop, fill=petit)) +
   geom_bar(stat="identity", position = "dodge")+
-  labs(x="", y="%", title="Figure 7: Petitioning by Socialising and Country", caption="ESS 2016")+
+  labs(x="", y="%", title="Figure 8: Petitioning by Socialising and Country", caption="Source: ESS 2002-2018")+
   theme_bw()+
-  facet_grid(~cntry)+
+  facet_grid(~country)+
   guides(fill=FALSE)
 
 
 # Demonstrating
 
-ESS %>% filter(!is.na(socialise), !is.na(demo)) %>%
-  group_by(socialise) %>% count(demo) %>%
+ess %>% filter(!is.na(strust), !is.na(demo)) %>%
+  group_by(strust) %>% count(demo) %>%
   mutate(prop=n/sum(n)*100) %>%
-  filter(!demo == "have not demonstrated") %>%
-  knitr::kable("pandoc", caption = "Socialising and Demonstrating",
+  filter(!demo == "not demonstrated") %>%
+  kbl(caption = "Socialising and Demonstrating",
   col.names = c('Socialising', 'Demonstrating', 'N', 'Percentage'),
-  digits=2, align="cccc") %>% print()
+  digits=2, align="cccc") %>%
+  kable_classic_2(full_width=F, position= "left")%>%
+  footnote(general = "Source: ESS 2002-2018")
 
 # Demonstrating Across Countries
 
-ESS %>% filter(!is.na(socialise), !is.na(demo)) %>%
-  group_by(socialise, cntry) %>% count(demo) %>%
+ess %>% filter(!is.na(strust), !is.na(demo)) %>%
+  group_by(strust, country) %>% count(demo) %>%
   mutate(prop=n/sum(n)*100) %>%
-  filter(!demo == "have not demonstrated") %>%
-  ggplot(aes(x=socialise, y=prop, fill=demo)) +
+  filter(!demo == "not demonstrated") %>%
+  ggplot(aes(x=strust, y=prop, fill=demo)) +
   geom_bar(stat="identity", position = "dodge")+
-  labs(x="", y="%", title="Figure 3: Demonstrating by Socialising and Country", caption="ESS 2016")+
+  labs(x="", y="%", title="Figure 9: Demonstrating by Social Trust and Country", caption="Source: ESS 2002-2018")+
   theme_bw()+
-  facet_grid(~cntry)+
+  facet_grid(~country)+
   guides(fill=FALSE)
 
 # So, time to draw some conclusions.
