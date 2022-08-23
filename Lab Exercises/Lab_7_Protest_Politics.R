e4f5e77cc48e2353575ddbdb8f1979753f6a4b65
Dr Ross Campbell <36095902+drcampbell1@users.noreply.github.com>
2022-08-23 11:47:55 +0100

    Update Lab_7_Protest_Politics.R

--- Lab Exercises/Lab_7_Protest_Politics.R
+++ Lab Exercises/Lab_7_Protest_Politics.R
@@ -1,60 +1,68 @@
 # As we've discussed in the lectures, there are other forms of protest politics.
-# In this lecture, we're going to focus on petitioning and boycotting.
+# In this lecture, we're going to focus on petiting and boycotting.
 # These are important because people may show their support for a group or cause by signing a public
-# petition supporting it, or they may boycott products for political or ethical reasons.
+# petit supporting it, or they may boycott products for political or ethical reasons.
 
 library(tidyverse)
 library(kableExtra)
 options(warn = -1)
 
-# Question 1: How common is petitioning in European democracies?
+# Question 1: How common is petiting in European democracies?
 
-ESS %>% filter(!is.na(petition)) %>% count(petition) %>%
+ess %>% filter(!is.na(petit)) %>% count(petit) %>%
   mutate(percent = round(n/sum(n)*100, digits=1)) %>%
-  knitr::kable("pandoc", caption = "Petitioning in European Democracies",
-               col.names = c('Petitioning', 'N', '%'), align="lcc")
+  kbl(caption = "petiting in European Democracies",
+               col.names = c('petiting', 'N', '%'), align="lcc")%>%
+  kable_classic_2(full_width=F, position= "left")%>%
+  footnote(general = "Source: ESS 2002-2018")
 
 # Let's break this down by the six countries for which we have data:
 
-ESS %>% filter(!is.na(petition)) %>% group_by(cntry) %>% count(petition) %>%
+ess %>% filter(!is.na(petit)) %>% group_by(country) %>% count(petit) %>%
   mutate(percent = round(n/sum(n)*100, digits=1)) %>%
-  knitr::kable("pandoc", caption = "Petitioning in European Democracies",
-               col.names = c('Country', 'Petitioning', 'N', '%'), align="lccc")
+  kbl(caption = "petiting in European Democracies",
+               col.names = c('Country', 'petiting', 'N', '%'), align="lccc")%>%
+  kable_classic_2(full_width=F, position= "left")%>%
+  footnote(general = "Source: ESS 2002-2018")
 
 # We can graph this to make it clear
 
-ESS %>% group_by(cntry) %>% filter(!is.na(petition)) %>%
-  count(petition) %>% mutate(prop=prop.table(n*100)) %>%
-  filter(!petition=="have not signed petition") %>%
-  ggplot(aes(x=reorder(cntry, -prop), y=prop)) +
+ess %>% group_by(country) %>% filter(!is.na(petit)) %>%
+  count(petit) %>% mutate(prop=prop.table(n*100)) %>%
+  filter(!petit=="not signed") %>%
+  ggplot(aes(x=reorder(country, -prop), y=prop)) +
   geom_bar(stat="identity")+
-  labs(x="", y="", title="Figure 1: Petitioning by Country", caption="ESS 2016")+
+  labs(x="", y="", title="Figure 1: petiting by Country", caption="Source: ESS 2002-2016")+
   scale_y_continuous(labels=scales::percent)+
   theme_bw()+
   guides(fill=FALSE)
 
 # Question 2: How common is boycotting in European democracies?
 
-ESS %>% filter(!is.na(boyct)) %>% count(boyct) %>%
+ess %>% filter(!is.na(boyct)) %>% count(boyct) %>%
   mutate(percent = round(n/sum(n)*100, digits=1)) %>%
-  knitr::kable("pandoc", caption = "Boycotting in European Democracies",
-               col.names = c('Boycotting', 'N', '%'), align="lcc")
+  kbl(caption = "Boycotting in European Democracies",
+               col.names = c('Boycotting', 'N', '%'), align="lcc")%>%
+  kable_classic_2(full_width=F, position= "left")%>%
+  footnote(general = "Source: ESS 2002-2018")
 
 # Let's break this down by the six countries for which we have data:
 
-ESS %>% filter(!is.na(boyct)) %>% group_by(cntry) %>% count(boyct) %>%
+ess %>% filter(!is.na(boyct)) %>% group_by(country) %>% count(boyct) %>%
   mutate(percent = round(n/sum(n)*100, digits=1)) %>%
-  knitr::kable("pandoc", caption = "Boycotting in European Democracies",
-               col.names = c('Country', 'Boycotting', 'N', '%'), align="lccc")
+  kbl("pandoc", caption = "Boycotting in European Democracies",
+               col.names = c('Country', 'Boycotting', 'N', '%'), align="lccc")%>%
+  kable_classic_2(full_width=F, position= "left")%>%
+  footnote(general = "Source: ESS 2002-2018")
 
 # We can graph this to make it clear
 
-ESS %>% group_by(cntry) %>% filter(!is.na(boyct)) %>%
+ess %>% group_by(country) %>% filter(!is.na(boyct)) %>%
   count(boyct) %>% mutate(prop=prop.table(n*100)) %>%
-  filter(!boyct=="have not boycotted") %>%
-  ggplot(aes(x=reorder(cntry, -prop), y=prop)) +
+  filter(!boyct=="not boycotted") %>%
+  ggplot(aes(x=reorder(country, -prop), y=prop)) +
   geom_bar(stat="identity")+
-  labs(x="", y="", title="Figure 2: Boycotting by Country", caption="ESS 2016")+
+  labs(x="", y="", title="Figure 2: Boycotting by Country", caption="Source: ESS 2002-2018")+
   scale_y_continuous(labels=scales::percent)+
   theme_bw()+
   guides(fill=FALSE)
@@ -64,24 +72,24 @@ ESS %>% group_by(cntry) %>% filter(!is.na(boyct)) %>%
 
 # What is the age profile of petitioners?
 
-ESS %>% group_by(agecat) %>% filter(!is.na(petition), !is.na(agecat)) %>%
-  count(petition) %>% mutate(prop=prop.table(n*100)) %>%
-  filter(!petition=="have not signed petition") %>%
+ess %>% group_by(agecat) %>% filter(!is.na(petit), !is.na(agecat)) %>%
+  count(petit) %>% mutate(prop=prop.table(n*100)) %>%
+  filter(!petit=="not signed") %>%
   ggplot(aes(x=agecat, y=prop)) +
   geom_bar(stat="identity")+
-  labs(x="", y="", title="Figure 3: Petitioning by Age Category", caption="ESS 2016")+
+  labs(x="", y="", title="Figure 3: petiting by Age Category", caption="Source: ESS 2002-2018")+
   scale_y_continuous(labels=scales::percent)+
   theme_bw()+
   guides(fill=FALSE)
 
 # What is the age profile of people who boycott?
 
-ESS %>% group_by(agecat) %>% filter(!is.na(boyct), !is.na(agecat)) %>%
+ess %>% group_by(agecat) %>% filter(!is.na(boyct), !is.na(agecat)) %>%
   count(boyct) %>% mutate(prop=prop.table(n*100)) %>%
-  filter(!boyct=="have not boycotted") %>%
+  filter(!boyct=="not boycotted") %>%
   ggplot(aes(x=agecat, y=prop)) +
   geom_bar(stat="identity")+
-  labs(x="", y="", title="Figure 4: Boycotting by Age Category", caption="ESS 2016")+
+  labs(x="", y="", title="Figure 4: Boycotting by Age Category", caption="ess 2016")+
   scale_y_continuous(labels=scales::percent)+
   theme_bw()+
   guides(fill=FALSE)
@@ -90,49 +98,49 @@ ESS %>% group_by(agecat) %>% filter(!is.na(boyct), !is.na(agecat)) %>%
 
 # What is the education profile of petitioners?
 
-ESS %>% group_by(educat) %>% filter(!is.na(petition), !is.na(educat)) %>%
-  count(petition) %>% mutate(prop=prop.table(n*100)) %>%
-  filter(!petition=="have not signed petition") %>%
+ess %>% group_by(educat) %>% filter(!is.na(petit), !is.na(educat)) %>%
+  count(petit) %>% mutate(prop=prop.table(n*100)) %>%
+  filter(!petit=="not signed") %>%
   ggplot(aes(x=educat, y=prop)) +
   geom_bar(stat="identity")+
-  labs(x="", y="", title="Figure 5: Petitioning by Education", caption="ESS 2016")+
+  labs(x="", y="", title="Figure 5: Petitioning by Education", caption="Source: ESS 2002-2016")+
   scale_y_continuous(labels=scales::percent)+
   theme_bw()+
   guides(fill=FALSE)
 
 # What is the education profile of people who boycott?
 
-ESS %>% group_by(educat) %>% filter(!is.na(boyct), !is.na(educat)) %>%
+ess %>% group_by(educat) %>% filter(!is.na(boyct), !is.na(educat)) %>%
   count(boyct) %>% mutate(prop=prop.table(n*100)) %>%
-  filter(!boyct=="have not boycotted") %>%
+  filter(!boyct=="not boycotted") %>%
   ggplot(aes(x=educat, y=prop)) +
   geom_bar(stat="identity")+
-  labs(x="", y="", title="Figure 6: Boycotting by Education", caption="ESS 2016")+
+  labs(x="", y="", title="Figure 6: Boycotting by Education", caption="Source: ESS 2002-2016")+
   scale_y_continuous(labels=scales::percent)+
   theme_bw()+
   guides(fill=FALSE)
 
 
-# What is the gender profile of petitioners?
+# What is the gender profile of petiters?
 
 # Males only?
 
-ESS %>% group_by(gender) %>% filter(!is.na(petition), !is.na(gender)) %>%
-  count(petition) %>% mutate(prop=prop.table(n*100)) %>%
-  filter(!petition=="have not signed petition") %>%
+ess %>% group_by(gender) %>% filter(!is.na(petit), !is.na(gender)) %>%
+  count(petit) %>% mutate(prop=prop.table(n*100)) %>%
+  filter(!petit=="not signed") %>%
   ggplot(aes(x=gender, y=prop)) +
   geom_bar(stat="identity")+
-  labs(x="", y="", title="Figure 7: Petitioning by Gender", caption="ESS 2016")+
+  labs(x="", y="", title="Figure 7: petiting by Gender", caption="ess 2016")+
   scale_y_continuous(labels=scales::percent)+
   theme_bw()+
   guides(fill=FALSE)
 
-ESS %>% group_by(gender) %>% filter(!is.na(boyct), !is.na(gender)) %>%
+ess %>% group_by(gender) %>% filter(!is.na(boyct), !is.na(gender)) %>%
   count(boyct) %>% mutate(prop=prop.table(n*100)) %>%
-  filter(!boyct=="have not boycotted") %>%
+  filter(!boyct=="not boycotted") %>%
   ggplot(aes(x=gender, y=prop)) +
   geom_bar(stat="identity")+
-  labs(x="", y="", title="Figure 8: Boycotting by Gender", caption="ESS 2016")+
+  labs(x="", y="", title="Figure 8: Boycotting by Gender", caption="Source: ESS 2002-2016")+
   scale_y_continuous(labels=scales::percent)+
   theme_bw()+
   guides(fill=FALSE)
