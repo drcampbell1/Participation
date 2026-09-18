831031a754efa194e57ab63c3c662b50864c3bf8
Dr Ross Campbell <36095902+drcampbell1@users.noreply.github.com>
2026-09-18 09:11:30 +0100

    Update Lab_2_Voting.R

--- Lab Exercises/Lab_2_Voting.R
+++ Lab Exercises/Lab_2_Voting.R
@@ -6,32 +6,32 @@ options(warn = -1)
 # Let's ask a simple question: do people vote?
 
 ess %>% 
-filter(!is.na(vote)) %>% 
-count(vote) %>%
+  filter(!is.na(vote)) %>% 
+  count(vote) %>%
   mutate("%" = round(n/sum(n) * 100, digits=1)) %>%
   kbl(caption = "Electoral Turnout in European Democracies",
-  col.names = c('voted', 'N', '%'), align="lccc") %>%
+      col.names = c('voted', 'N', '%'), align="lccc") %>%
   kable_classic_2(full_width=F, position= "left")%>%
   footnote(general = "Source: ESS 2002-2018")
 
 # How does this vary by country?#
 
 ess %>% 
-group_by(country) %>% 
-filter(!is.na(vote)) %>%
+  group_by(country) %>% 
+  filter(!is.na(vote)) %>%
   count(vote) %>% 
-mutate("%" = round(n/sum(n) * 100, digits=1)) %>%
+  mutate("%" = round(n/sum(n) * 100, digits=1)) %>%
   kbl(caption = "Electoral Turnout in European Democracies",
-  col.names = c('Country', 'Voted', 'N', '%'), align="cccc")%>% 
+      col.names = c('Country', 'Voted', 'N', '%'), align="cccc")%>% 
   kable_classic_2(full_width=F, position= "left") %>% 
   footnote("European Social Survey, 2002-2018")
 
 # We can present this graphically to make it clearer#
 ess %>% 
-group_by(country) %>% 
-filter(!is.na(vote)) %>%
+  group_by(country) %>% 
+  filter(!is.na(vote)) %>%
   count(vote) %>% 
-mutate(prop=prop.table(n*100)) %>%
+  mutate(prop=prop.table(n*100)) %>%
   filter(!vote=="did not vote") %>%
   ggplot(aes(x=reorder(country, -prop), y=prop)) +
   geom_col()+
@@ -41,15 +41,15 @@ mutate(prop=prop.table(n*100)) %>%
        caption="Source: ESS 2002-2018")+
   scale_y_continuous(labels=scales::percent)+
   theme_bw()
- 
+
 
 # What's changed over time? 
 
 ess %>% 
-group_by(year, country) %>% 
-filter(!is.na(vote)) %>%
+  group_by(year, country) %>% 
+  filter(!is.na(vote)) %>%
   count(vote) %>% 
-mutate(prop=prop.table(n*100)) %>%
+  mutate(prop=prop.table(n*100)) %>%
   filter(!vote=="did not vote") %>%
   ggplot(aes(year, prop))+
   geom_line()+
@@ -68,41 +68,41 @@ mutate(prop=prop.table(n*100)) %>%
 # What about Education?#
 
 ess %>% 
-group_by(educat) %>% 
-filter(!is.na(vote), !is.na(educat)) %>%
+  group_by(educat) %>% 
+  filter(!is.na(vote), !is.na(educat)) %>%
   count(vote) %>% 
-mutate("%" =n/sum(n)*100) %>%
+  mutate("%" =n/sum(n)*100) %>%
   kbl(caption = "Education and Electoral Turnout",
-  col.names = c('Education', 'Voted', 'N', '%'), align="cccc", digits=1)%>% 
+      col.names = c('Education', 'Voted', 'N', '%'), align="cccc", digits=1)%>% 
   kable_classic_2(full_width=F, position= "left") %>% 
   footnote("European Social Survey, 2002-2018")
 
 # We can graph the relationship
 
 ess %>% 
-group_by(educat) %>% 
-filter(!is.na(vote), !is.na(educat)) %>%
+  group_by(educat) %>% 
+  filter(!is.na(vote), !is.na(educat)) %>%
   count(vote) %>% 
-mutate(prop=prop.table(n*100)) %>% 
-mutate("%" =prop*100) %>%
+  mutate(prop=prop.table(n*100)) %>% 
+  mutate("%" =prop*100) %>%
   ggplot(aes(x=educat, y=prop, fill=vote))+
   labs(x="", y="", title="Figure 2: Turnout by Education", caption="ESS 2002-2018")+
   scale_fill_manual(values=c("darkred", "steelblue"))+
   geom_col(position = "dodge")+
   scale_y_continuous(labels=scales::percent)+
   theme_bw()+
-  theme(legend.title = element_blank())+
-  theme(legend.position = "bottom")
+  theme(legend.title = element_blank(),
+        legend.position = "bottom")
 
 
 # Turnout by Education and Country #
 
 ess %>% 
-group_by(educat, country) %>% 
-filter(!is.na(vote), !is.na(educat)) %>%
+  group_by(educat, country) %>% 
+  filter(!is.na(vote), !is.na(educat)) %>%
   count(vote) %>% 
-mutate(prop=prop.table(n*100)) %>% 
-mutate("%" =prop*100) %>%
+  mutate(prop=prop.table(n*100)) %>% 
+  mutate("%" =prop*100) %>%
   ggplot(aes(x=educat, y=prop, fill = vote))+
   labs(x="", y="", title="Figure 3: Turnout by Education and Country", caption="Source: ESS 2002-2018")+
   geom_col(position = "dodge")+
@@ -110,22 +110,22 @@ mutate("%" =prop*100) %>%
   facet_wrap(~country, nrow=3)+
   scale_y_continuous(labels=scales::percent)+
   theme_bw()+
-  theme(legend.title = element_blank())+
-  theme(legend.position = "bottom")
+  theme(legend.title = element_blank(),
+        legend.position = "bottom")
 
 # Q2: Does Economic Rationality Influence Turnout?
 
 ess %>% 
-group_by(quin) %>% 
-filter(!is.na(vote), !is.na(quin)) %>%
+  group_by(quin) %>% 
+  filter(!is.na(vote), !is.na(quin)) %>%
   count(vote) %>% 
-mutate(prop= n /sum(n)*100) %>%
-filter(vote == 'voted') %>%
+  mutate(prop= n /sum(n)*100) %>%
+  filter(vote == 'voted') %>%
   kbl(caption = "Income and Electoral Turnout",
-  col.names=c('Income Quintile', 
-              'Voted', 
-              'N', 
-              '%'), 
+      col.names=c('Income Quintile', 
+                  'Voted', 
+                  'N', 
+                  '%'), 
       align="cccc", digits=1)%>% 
   kable_classic_2(full_width=F, position= "left") %>% 
   footnote("European Social Survey, 2002-2018")
@@ -133,29 +133,29 @@ filter(vote == 'voted') %>%
 # We can graph this relationship
 
 ess %>% 
-group_by(quin) %>% 
-filter(!is.na(vote), !is.na(quin)) %>%
+  group_by(quin) %>% 
+  filter(!is.na(vote), !is.na(quin)) %>%
   count(vote) %>% 
-mutate(prop=prop.table(n*100)) %>% 
-mutate("%" =prop*100) %>%
+  mutate(prop=prop.table(n*100)) %>% 
+  mutate("%" =prop*100) %>%
   ggplot(aes(x=quin, y=prop, fill = vote))+
   labs(x="", y="", title="Figure 4: Turnout by Perceptions of the Economy", caption="Source: ESS 2002-2018")+
   geom_col(position = "dodge")+
   scale_fill_manual(values=c("darkred", "steelblue"))+
   scale_y_continuous(labels=scales::percent)+
   theme_bw()+
-  theme(legend.title = element_blank())+
-  theme(legend.position = "bottom")
+  theme(legend.title = element_blank(),
+        legend.position = "bottom")
 
 
 # Does it differ between the countries?
 
 ess %>% 
-group_by(quin, country) %>% 
-filter(!is.na(vote), !is.na(quin)) %>%
+  group_by(quin, country) %>% 
+  filter(!is.na(vote), !is.na(quin)) %>%
   count(vote) %>% 
-mutate(prop=prop.table(n*100)) %>% 
-mutate("%" =prop*100) %>%
+  mutate(prop=prop.table(n*100)) %>% 
+  mutate("%" =prop*100) %>%
   ggplot(aes(x=quin, y=prop, fill = vote))+
   labs(x="", 
        y="", 
@@ -166,8 +166,8 @@ mutate("%" =prop*100) %>%
   facet_wrap(~country, nrow=3)+
   scale_y_continuous(labels=scales::percent)+
   theme_bw()+
-  theme(legend.title = element_blank())+
-  theme(legend.position = "bottom")
+  theme(legend.title = element_blank(),
+        legend.position = "bottom")
 
 #Q3 Does history matter? Does the legacy of post-communism influence participation?
 
@@ -199,27 +199,27 @@ ess %>%
 # We can do this by comparing the percentage of young people found in the sample with the percentage found amongst the voters?
 
 a <- ess %>% 
-filter(!is.na(age)) %>% 
-mutate(generation = if_else(age >= 18 & age <= 25, "18-25", "26+")) %>% 
-count(generation) %>% 
-mutate(percent = round(n /sum(n)*100, digits = 1)) 
+  filter(!is.na(age)) %>% 
+  mutate(generation = if_else(age >= 18 & age <= 25, "18-25", "26+")) %>% 
+  count(generation) %>% 
+  mutate(percent = round(n /sum(n)*100, digits = 1)) 
 
 b <- ess %>% 
-filter(!is.na(age)) %>% 
-mutate(generation = if_else(age >= 18 & age <= 25, "18-25", "26+")) %>% 
-filter(vote == "voted") %>% 
-group_by(generation) %>% 
-count(vote) %>% 
-ungroup() %>% 
-mutate(percent = round(n/sum(n)*100, digits = 1))
+  filter(!is.na(age)) %>% 
+  mutate(generation = if_else(age >= 18 & age <= 25, "18-25", "26+")) %>% 
+  filter(vote == "voted") %>% 
+  group_by(generation) %>% 
+  count(vote) %>% 
+  ungroup() %>% 
+  mutate(percent = round(n/sum(n)*100, digits = 1))
 
 left_join(a, b, by = "generation") %>% 
-select(-c(n.x, n.y, vote)) %>% 
-kbl(caption = "Percentage of Age within Sample and Percentage of Age Cohort amongst Voters",
-             col.names = c('Generation', 
-                           'Percentage Within Sample', 
-                           'Percentage Within Voters'), 
-             align="ccc") %>% 
+  select(-c(n.x, n.y, vote)) %>% 
+  kbl(caption = "Percentage of Age within Sample and Percentage of Age Cohort amongst Voters",
+      col.names = c('Generation', 
+                    'Percentage Within Sample', 
+                    'Percentage Within Voters'), 
+      align="ccc") %>% 
   kable_classic_2(full_width=F, position= "left") %>% 
   footnote("European Social Survey, 2002-2018")
 
@@ -227,8 +227,11 @@ rm(a,b)
 
 # Three questions to conclude with:
 
-      #So, thinking over the theories we've look at: which ones work?
+#So, thinking over the theories we've look at: which ones work?
 
+#Do all work equally well in all countries?
+
+#Does it matter that young people seem less likely to vote?
       #Do all work equally well in all countries?
 
       #Does it matter that young people seem less likely to vote?
